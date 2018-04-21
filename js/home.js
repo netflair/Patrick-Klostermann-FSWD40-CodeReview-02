ecd4a0f23160c9563eca8bc68c6fc5f23125f3a7
netflair <dev@netflair.at>
2018-04-21 15:07:58 +0200

    Some additional styleing; Better comments in .js-files

--- js/home.js
+++ js/home.js
@@ -1,29 +1,58 @@
-//Change Invoice in header
+//Variables----------------------------------------------------------------
+//Get Containers for Output
 var tokio = document.getElementById('tokio');
 var moskau = document.getElementById('moskau');
 var invoice = document.getElementById('sum');
 
-//Listen for click, execute function
-tokio.addEventListener("click", changeInvoice);
-moskau.addEventListener("click", changeInvoice);
+//Get Containers for added Classes
+var tokio_label = document.getElementById('tokio_label');
+var moskau_label = document.getElementById('moskau_label');
+var tokio_img = document.getElementById('tokio_img');
+var moskau_img = document.getElementById('moskau_img');
+var tokio_info = document.getElementById('tokio_info');
+var moskau_info = document.getElementById('moskau_info');
 
-function changeInvoice() {
-    //if checked assign t value
+
+//Listen for checkbox cklick, execute function
+tokio.addEventListener("click", setState);
+moskau.addEventListener("click", setState);
+
+function setState() {
+    //Tokio---------------------------------------------------------------
+
+    //if checked set t value, and add classes to containers
     if (tokio.checked == true) {
         t = 2436;
+        tokio_label.classList.add("label_checked");
+        tokio_img.classList.add("img_checked");
+        tokio_info.classList.add("checked");
     }
-    //if not assign different value
+    //if not checked reset t value, and remove classes from containers
     else {
         t = 0;
+        tokio_label.classList.remove("label_checked");
+        tokio_img.classList.remove("img_checked");
+        tokio_info.classList.remove("checked");
     }
-    //if checked assign m value
+
+
+    //Moskau---------------------------------------------------------------
+
+    //if checked set m value, and add classes to containers
     if (moskau.checked == true) {
         m = 2287;
+        moskau_label.classList.add("label_checked");
+        moskau_img.classList.add("img_checked");
+        moskau_info.classList.add("checked");
     }
-    //if not assign different value
+    //if not checked reset m value, and remove classes from containers
     else {
         m = 0;
+        moskau_label.classList.remove("label_checked");
+        moskau_img.classList.remove("img_checked");
+        moskau_info.classList.remove("checked");
     }
-    //Output the sum of both in the header
+
+    //Output the sum of both in the header -------------------------------
     invoice.innerHTML = t + m;
 }
--- js/invoice.js
+++ js/invoice.js
@@ -1,10 +1,12 @@
-//Variables
+//Variables-----------------------------------------------------------
+//Get Containers for Output.innerHTML
 var sum2 = document.getElementById('sum2');
 var price_tokio = document.getElementById('price_tokio');
 var price_moskau = document.getElementById('price_moskau');
 var table_sum1 = document.getElementById('table_sum1');
 var golden_check = document.getElementById('golden_check');
 var save = document.getElementById('save');
+//As asked in the Code Review (Names and Calculations)
 var tokio = "Tokio";
 var moskau = "Moskau";
 var tokioPrice = 2436;
@@ -13,39 +15,41 @@ var sum = tokioPrice + moskauPrice;
 var golden = sum - sum / 10;
 var saveings = sum / 10;
 
-//Change Invoice in header
+//Change Invoice in header--------------------------------------------
 sum2.innerHTML = sum;
 save.innerHTML = saveings + "0 €";
 
-//Info Output with document.write()
+//Info Output with document.write()-----------------------------------
 document.write(
     "<div id='invoice'><p>If you want to buy a <b>Flight to " + tokio +
     "</b> and a <b>Flight to " + moskau +
     "</b>, it costs:</p><p><b>" + sum + " EUR</b></p></div>"
 );
 
-//Output for Booking
+//Output for Booking--------------------------------------------------
 price_tokio.innerHTML = tokioPrice + "€";
 price_moskau.innerHTML = moskauPrice + "€";
-table_sum1.innerHTML = sum + "€";
+table_sum1.innerHTML = "<b>" + sum + "€</b>";
 
 
-//Recalculated Output
+//Recalculated Output -------------------------------------------------
+//Listen if tick is set for Golden Card
 golden_check.addEventListener("click", changeInvoice);
 
+//Change 
 function changeInvoice() {
-    //if checked assign t value
+    //if checked fill containers with recalculated values
     if (golden_check.checked == true) {
         sum2.innerHTML = golden + "0";
         price_tokio.innerHTML = tokioPrice - tokioPrice / 10 + "0 €";
         price_moskau.innerHTML = moskauPrice - moskauPrice / 10 + "0 €";
-        table_sum1.innerHTML = golden + "0 €";
+        table_sum1.innerHTML = "<b>" + golden + "0 €</b>";
     }
-    //if not assign different value
+    //if not checked fill containers with default values again
     else {
         sum2.innerHTML = sum;
         price_tokio.innerHTML = tokioPrice + "€";
         price_moskau.innerHTML = moskauPrice + "€";
-        table_sum1.innerHTML = sum + "€";
+        table_sum1.innerHTML = "<b>" + sum + "€</b>";
     }
 }
